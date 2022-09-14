a2dcdd327b6f42b6fb5f4afa99cdcfa54224ef2d
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-09-14 10:55:37 +0900

    Initial Commit

---
+++ BlurBackgroundApp/BlurBackgroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurBackgroundAppApp.swift
+//  BlurBackgroundApp
+//
+//  Created by YoungK on 2022/09/14.
+//
+
+import SwiftUI
+
+@main
+struct BlurBackgroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
