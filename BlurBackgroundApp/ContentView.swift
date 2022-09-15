30e2e1dc220fde4506ed4716655f4adf72c86130
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-09-15 18:31:28 +0900

    [CHORE] 이미지 에셋 추가

--- BlurBackgroundApp/ContentView.swift
+++ BlurBackgroundApp/ContentView.swift
@@ -7,10 +7,45 @@
 
 import SwiftUI
 
+class BlurBackgroundViewModel: ObservableObject {
+    var images = ["0", "1", "2", "3"]
+    @Published var index = 0
+    
+    func nextImage() {
+        index = (index + 1) % images.count
+    }
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = BlurBackgroundViewModel()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            BlurBackgroundView(backgroundImage: "1") {
+                Spacer()
+                Image(viewModel.images[viewModel.index])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .onTapGesture {
+                        viewModel.nextImage()
+                    }
+                Spacer()
+            }
+        }
+    }
+}
+
+public struct BlurBackgroundView<Content: View>: View {
+    private var backgroundImage: String
+    private var content: () -> Content
+
+    init(backgroundImage: String, @ViewBuilder content: @escaping () -> Content) {
+        self.backgroundImage = backgroundImage
+        self.content = content
+    }
+    
+    public var body: some View {
+        content()
+            .background(Color.gray)
     }
 }
 
