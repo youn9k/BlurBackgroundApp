5e8d1647fe1a5342286dd56fa3477a7560a47f58
Youngking0914 <60254939+Youngking0914@users.noreply.github.com>
2022-09-28 11:31:35 +0900

    [UPDATE] 블러백그라운드

--- BlurBackgroundApp/ContentView.swift
+++ BlurBackgroundApp/ContentView.swift
@@ -7,45 +7,48 @@
 
 import SwiftUI
 
-class BlurBackgroundViewModel: ObservableObject {
-    var images = ["0", "1", "2", "3"]
-    @Published var index = 0
+struct ContentView: View {
+    let images = ["0", "1", "2", "3"]
+    @State var index: Int = 0
+    @State var image: String = "0"
+    
+    var body: some View {
+        VStack {
+            BlurBackgroundView(background: Image(image)) {
+                Image(image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .onTapGesture {nextImage()}
+            }
+        }
+    }
     
     func nextImage() {
         index = (index + 1) % images.count
-    }
-}
-
-struct ContentView: View {
-    @StateObject var viewModel = BlurBackgroundViewModel()
-    var body: some View {
-        VStack {
-            BlurBackgroundView(backgroundImage: "1") {
-                Spacer()
-                Image(viewModel.images[viewModel.index])
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .onTapGesture {
-                        viewModel.nextImage()
-                    }
-                Spacer()
-            }
-        }
+        image = images[index]
     }
 }
 
 public struct BlurBackgroundView<Content: View>: View {
-    private var backgroundImage: String
+    private var background: Image
+    private var blur: CGFloat = 20
     private var content: () -> Content
 
-    init(backgroundImage: String, @ViewBuilder content: @escaping () -> Content) {
-        self.backgroundImage = backgroundImage
+    init(background: Image, blur: CGFloat, @ViewBuilder content: @escaping () -> Content) {
+        self.background = background
+        self.blur = blur
         self.content = content
     }
     
     public var body: some View {
-        content()
-            .background(Color.gray)
+        GeometryReader { proxy in
+            ZStack {
+                background
+                    .blur(radius: blur)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+                content()
+            }
+        }
     }
 }
 
